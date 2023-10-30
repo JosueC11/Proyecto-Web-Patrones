54509f2315b52b98b321ec83155c5e8bb7ec7925
Dennis Sánchez Morales <97916956+Saturnio07@users.noreply.github.com>
2023-10-29 19:43:06 -0600

    Formularios de login y registro agregados

--- src/main/java/com/clinica/controller/UsuarioLoginController.java
+++ src/main/java/com/clinica/controller/UsuarioLoginController.java
@@ -1,9 +1,19 @@
 package com.clinica.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  *
  * @author Dennis
  */
+@Controller
+@RequestMapping("/usuario")
 public class UsuarioLoginController {
     
-}
+    @GetMapping("/login")
+    public String inicioRegistro() {
+        return "/usuario/login";
+    }
+}    
--- src/main/java/com/clinica/service/impl/UsuarioLoginServiceImpl.java
+++ src/main/java/com/clinica/service/impl/UsuarioLoginServiceImpl.java
@@ -1,18 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.clinica.service.impl;
 
-/**
- *
- * @author Josuu
- */
 
 import com.clinica.dao.UsuarioLoginDao;
 import com.clinica.service.UsuarioLoginService;
-import org.springframework.stereotype.Service;
 
+
+/**
+ *
+ * @author Dennis
+ */
 
 public class UsuarioLoginServiceImpl implements UsuarioLoginService
 {
